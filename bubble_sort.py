546e9db14feec42c7a82c31b3b75c0aa7e7f2956
dongchol <49258960+dongchol@users.noreply.github.com>
2022-07-16 00:57:21 +0900

    Update bubble_sort.py

--- bubble_sort.py
+++ bubble_sort.py
@@ -10,8 +10,8 @@ def comp(a,b):
 input_lst = [7,4,5,1,3,10,5,7,16,2,24]
 
 for i in range(0, len(input_lst)):
-    for j in range(i+1, len(input_lst)):
-        input_lst[i], input_lst[j] = comp(input_lst[i], input_lst[j])
+    for j in range(0, len(input_lst)-i-1):
+        input_lst[j+1], input_lst[j] = comp(input_lst[j+1], input_lst[j])
 print(input_lst)
 
 #중첩 for문을 작성하기 때문에 정렬 알고리즘 시간 복잡도는 O(n^2)이다. best/avg/worst 전부 같은 시간 복잡도
