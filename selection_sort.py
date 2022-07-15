5e846ef3a0af89b76cf31d08051d4ad6d7f20a44
dongchol <49258960+dongchol@users.noreply.github.com>
2022-07-16 00:53:07 +0900

    Update selection_sort.py

--- selection_sort.py
+++ selection_sort.py
@@ -28,12 +28,14 @@ for i in range(0, len(input_lst)):
     min_idx = i
     for j in range(i+1, len(input_lst)):
         if input_lst[j] < input_lst[min_idx]:
-            input_lst[j], input_lst[min_idx] = input_lst[min_idx], input_lst[j]
-
+            min_idx = j
+    input_lst[i], input_lst[min_idx] = input_lst[min_idx], input_lst[i]
 
 print(input_lst)
 # 문제 해결 방향은 맞았으나, 인덱스를 사용하느냐 값을 받아 인덱스로 다시 변환하느냐의 차이로 난이도가 매우 달라짐
 # 또한 값을 받아 다시 인덱스로 변환하게 될경우 구현한 swap 함수가 적용되지 않음. 따라서 인덱스를 사용하는것이 훨씬 간단함
+# bubble sort 처럼 매번 바꾸는 것이 아니라 최소값을 찾아 해당 값만 위치 변동이 이루어짐
+
 #for i in range(0, len(input_lst)):
 #    k = input_lst[i]
 #    t = input_lst[input_lst.index(min_num(input_lst[i:]))]
